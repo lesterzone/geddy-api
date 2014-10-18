999946846cf614fdf9934cf1e97b7be22981b6af
lesterzone <lesterzone@gmail.com>
2014-10-17 20:47:17 -0600

    Added Support for CORS
    Added:
    
    * CORS configuration, Router Application Controller
    * send function on Application controller
    * jsdoc documentation on each modified file
    
    Changed:
    
    * .gitignore to ignore out/ folder used for generated documentation

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,56 @@
-var Application = function() {};
+'use strict';
+/**
+ * @class Application Controller
+ * @name Application
+ **/
+var Application = function() {
+
+    /**
+     * @property respondWith
+     * @name respondWith
+     * @type Array
+     * @memberof Application
+     **/
+    this.respondWith = ['json'];
+
+    /**
+     * @description Set CORS
+     * @name before
+     * @function
+     * @memberof Application
+     **/
+    this.before(function() {
+        var resp = this.response;
+        var inf = {
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With, authtoken',
+            'Access-Control-Max-Age': 5184000 //2 months
+        };
+
+        /**
+         * Set headers
+         **/
+        resp.setHeaders(200, inf);
+
+        if ('OPTIONS' === this.request.method) {
+            resp.finish();
+            return;
+        }
+    });
+
+    /**
+     * @description Express <b>like</b> send function, returns a json response
+     * @name send
+     * @function
+     * @memberof Application
+     **/
+    this.send = function(code, data) {
+        this.respond(data, {
+            statusCode: code
+        });
+    };
+};
 
 exports.Application = Application;
--- config/development.js
+++ config/development.js
@@ -1,13 +1,37 @@
 /**
- * Geddy Configuration for development environment
+ * @namespace
+ * @name Development
+ * @description Geddy Configuration for development environment
  **/
 var config = {
-    appName: 'Geddy Based API (development)',
+
+    /**
+     * @type String
+     * @memberof Development
+     * @description Name for this project under development environment
+     **/
+    appName: 'GeddyJS Based API (Dev)',
+
+    /**
+     * @type Boolean
+     * @memberof Development
+     * @default true
+     * @description Display full error messages or not?
+     **/
     detailedErrors: true,
+
+    /**
+     * @type Boolean
+     * @memberof Development
+     * @default true
+     * @description enable debug mode?
+     **/
     debug: true,
 
     /**
-     * Watch only those files that affect API files
+     * @type Object
+     * @memberof Development
+     * @description Watch only those files that affect API file
      **/
     watch: {
         files: [
@@ -16,19 +40,37 @@ var config = {
         includePattern: '\\.(js)$',
         excludePattern: '\\.git|node_modules'
     },
+
+    /**
+     * @type null
+     * @memberof Development
+     * @default null
+     * @description Which host name is used?
+     **/
     hostname: null,
+
+    /**
+     * @type Integer
+     * @memberof Development
+     * @default 4000
+     * @description Port number to be used on development
+     **/
     port: 4000,
 
     /**
-     * For now we are going to use default configuration, later on we will add
-     * support for Database
+     * @type Object
+     * @memberof Development
+     * @description For now we are going to use default configuration,
+     *              later on we will add support for Database
      **/
     model: {
         defaultAdapter: 'filesystem'
     },
 
     /**
-     * This is an API, we dont need sessions
+     * @type null
+     * @memberof Development
+     * @description This is an API, we don't need sessions
      **/
     sessions: null
 };
